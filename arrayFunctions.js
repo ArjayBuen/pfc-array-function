c24a50c1245d6cd1399b4c9d913f92e36822d4e4
ArjayBuen <arjaybuen10@gmail.com>
2023-11-23 21:15:43 +0800

    learning concat

---
+++ arrayFunctions.js
@@ -0,0 +1,14 @@
+
+let phrase="Learning to code is";
+let word="worthwhile";
+
+console.log(phrase.concat(" ",word));
+
+let array1=["HTML","CSS","JS"];
+let array2=["Bootstrap","React","Tailwind","Typescript"];
+console.log(array1.concat(array2));
+
+let array3=[1,2,3];
+let array4=[4,5,6];
+let array5=[7,,9];
+console.log(array3.concat(array4,array5));
